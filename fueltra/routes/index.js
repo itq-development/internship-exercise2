2e197829009c3f4988a3d8978109bb003ca15ef0
Vlad Zamskoi <v.zamskoi@gmail.com>
2015-11-22 00:26:18 +0300

    separate-result-page branch inited

--- fueltra/routes/index.js
+++ fueltra/routes/index.js
@@ -1,23 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var unitsToCountAverage = 100;
+var unitType = 'Kilometers'
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'Fueltra: fuel tracker',
-        purpose: 'Know how much fuel you spend per every 100',
-        units: 'kilometers'
+        unitType: unitType,
+        unitsNumber: unitsToCountAverage
     });
 });
 
-function fuelConsumptionPerUnit(fuel, length) {
-    return (fuel / length);
+function fuelConsumptionPerUnit(fuel, unitsNumber) {
+    return (fuel / unitsNumber);
 }
 
 router.post('/', function (req, res) {
-    var fuel = req.body.fuel;
-    var length = req.body.length
-    console.log(fuelConsumptionPerUnit(fuel, length) * 100);
+    var fuel = req.body['fuel-value'];
+    var length = req.body['journey-length'];
+
+    res.render('average-consumption', {
+        unitType: unitType,
+        unitsNumber: unitsToCountAverage,
+        consumptionAverage: fuelConsumptionPerUnit(fuel, length) * unitsToCountAverage
+    });
+    //console.log(fuelConsumptionPerUnit(fuel, length) * 100);
+    //console.log(req.body);
 });
 
 module.exports = router;
---
+++ fueltra/routes/result.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+//
+///* GET result page. */
+//router.get('/', function (req, res, next) {
+//    var unitsToCountAverage = req.body.unitsToCountAverage;
+//    var unitType = req.body.unitType;
+//    res.render('average-consumption', {
+//        unitType: unitType,
+//        unitsNumber: unitsToCountAverage,
+//        consumptionAverage: fuelConsumptionPerUnit(fuel, length) * unitsToCountAverage
+//    });
+//});
+//
+//function fuelConsumptionPerUnit(fuel, unitsNumber) {
+//    return (fuel / unitsNumber);
+//}
+//
+module.exports = router;

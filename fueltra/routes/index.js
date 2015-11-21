53cfc9fa7c3cab6db23e4741f5bab68cf0dea7ca
Vlad Zamskoi <v.zamskoi@gmail.com>
2015-11-21 20:49:20 +0300

    application home page with jorney-data-input form is created
    Logic is here:
        fueltra/routes/index.js
    
    View is here:
        fueltra/views/index.jade
    
    Base layout modified:
        fueltra/public/stylesheets/style.css

--- fueltra/routes/index.js
+++ fueltra/routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Fueltra: fuel tracker',
+        purpose: 'Know how much fuel you spend per every 100',
+        units: 'kilometers'
+    });
+});
+
+function fuelConsumptionPerUnit(fuel, length) {
+    return (fuel / length);
+}
+
+router.post('/', function (req, res) {
+    var fuel = req.body.fuel;
+    var length = req.body.length
+    console.log(fuelConsumptionPerUnit(fuel, length) * 100);
 });
 
 module.exports = router;
